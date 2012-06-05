016e8118042a0214d984c76c26714b8ed37fd689
Martin Fleischer <fleischer.martin@gmx.net>
2012-06-05 17:54:47 +0200

    .gitignore

--- Uebung3/pair.h
+++ Uebung3/pair.h
@@ -1,24 +1,36 @@
 #ifndef PAIR_H
 #define PAIR_H
+
 #include <iostream>
 
+
+namespace mystl {
+
 template <typename T1, typename T2>
-
 class Pair{
-
-
-public:
-    Pair(T1 first = T1(),T2 second = T2());
-    T1& first();
-    T2& second();
-    T1& first() const;
-    T2& second() const;
-    std::ostream &operator<<(std::ostream &ostr, Pair &pair);
-
-private:
     T1 m_first;
     T2 m_second;
 
+public:
+    Pair(T1 first = T1(),T2 second = T2());
+    T1& first(){
+        return m_first;
+    }
+
+    T2& second(){
+        return m_second;
+    }
+
+    T1& first() const{
+        return m_first;
+    }
+    T2& second() const{
+        return m_second;
+    }
 };
+
+inline std::ostream& operator<<(std::ostream &lhs, const Pair &pair);
+
+}
 
 #endif // PAIR_H
