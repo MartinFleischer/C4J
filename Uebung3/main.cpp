30a3f75123484bfcd17c55aaba219e54afc05d87
Martin Fleischer <fleischer.martin@gmx.net>
2012-06-12 18:04:04 +0200

    bla

--- Uebung3/main.cpp
+++ Uebung3/main.cpp
@@ -103,8 +103,8 @@ int main()
     t.insert(2);
     t.insert(1);
     t.insert(5);
-    cout << endl << (t.end().m_node == 0)  << endl;
-    cout << "tree 4-3-2-1-5: " << printAndCount(t);
+    cout << endl << t.m_root->left()->value()  << endl;
+    //cout << "tree 4-3-2-1-5: " << printAndCount(t);
     //assert(printAndCount(t) == 5);
 
 #if 0 // move this line down while your implementation proceeds...
